0fe832cd5838e3c0ee422d33c1641a39302b941a
Daniel <d13751200344@gmail.com>
2022-04-13 18:20:10 +0800

    pattern routing

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const app = express();
 
-app.get("/", function (req, res) {
-    res.send("You are on the homepageURL.");
-})
+app.get("/fruit/:someFruit", (req, res) => {
+		res.send (`You are looking for ${req.params.someFruit}.`)
+});
 
 app.listen(3000, ()=> {
     console.log("Server is running on port 3000.")
-})
+});
